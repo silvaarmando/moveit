2dac662e3bfe554dca590a3c196a9b1324b0c56e
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-02-22 23:20:11 -0300

    Learn Props and Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Button } from './components/Button';
+
 function App() {
   return (
-    <h1>
-      Hello React World!
-    </h1>
+    <div>
+      <Button color="red">
+        Botão 1
+      </Button>
+      <Button color="green">
+        Botão 2
+      </Button>
+      <Button color="blue">
+        Botão 3
+      </Button>
+    </div>
   );
 }
 
